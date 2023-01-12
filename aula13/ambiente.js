f4910f652424e55c57121e08bb75d3504057e024
Bruno Barreto da Cunha Holanda <bbarretoch@gmail.com>
2023-01-11 21:11:02 -0300

    aula 13 curso de JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var contador = 1
+
+/*while (contador <= 6) {
+    console.log(`Passo ${contador}`)//o que eu quero executar enquanto a condição do while for verdadeira
+    contador++ //aumentar o valor do contador
+}*/
+
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
