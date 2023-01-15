77c277a352b1cf2e337d86c86eb03df6a858a94a
Bruno Barreto da Cunha Holanda <114125291+BBarretoCH@users.noreply.github.com>
2023-01-14 22:22:57 -0300

    Aula15-functions do curso de JS

---
+++ aula16-Funções/funcoes.js
@@ -0,0 +1,17 @@
+function parimp(n) {
+    if (n%2 ==0 ) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+
+    if(m%2 == 0) {
+        return 'Par'
+    } else {
+        return 'ímpar'
+    }
+}
+
+let res = parimp(11)
+
+console.log(res)
